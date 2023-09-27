6562d518d1507580a6b09ed23baca2f05570e205
7eta <joon7eta@gmail.com>
2023-09-27 11:37:41 +0900

    수정

--- run_nerf.py
+++ run_nerf.py
@@ -1222,8 +1222,8 @@ def train():
             with torch.no_grad():
                 generate_and_write_mesh(global_step, 
                                         bounding_box, 
-                                        poses[i_train].cpu().numpy(), 
-                                        np.array(imgs_path)[i_train], 
+                                        poses.cpu().numpy(), 
+                                        np.array(imgs_path), 
                                         hwf, 
                                         num_pts, 
                                         levels, 
