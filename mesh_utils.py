b319445074e38ce11afe97f95a36406f858816a2
7eta <joon7eta@gmail.com>
2023-10-01 18:08:21 +0900

    수정

--- mesh_utils.py
+++ mesh_utils.py
@@ -287,8 +287,8 @@ def convert_sigma_samples_to_ply(
 
         weights = raw2outputs(raw, z_vals.cuda(), rays_d.cuda())
         
-        opacity = weights[:, np.newaxis] # (N_vertices, 1) -?확인됨
-        opacity = np.nan_to_num(opacity.cpu().numpy(), 1)
+        opacity = weights.cpu().numpy()[:, np.newaxis] # (N_vertices, 1) -?확인됨
+        opacity = np.nan_to_num(opacity, 1)
             
         non_occluded = np.ones_like(non_occluded_sum) * 0.1/depth
         non_occluded += opacity < 0.2
