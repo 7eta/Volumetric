bf5476ce37516de834dc9a3d643dbe6043f0ff70
7eta <joon7eta@gmail.com>
2023-10-01 19:55:57 +0900

    수정

--- mesh_utils.py
+++ mesh_utils.py
@@ -200,13 +200,13 @@ def convert_sigma_samples_to_ply(
         # rays = torch.cat([rays_o, rays_d, near, far], 1).cuda()
         # print(f"!!! rays.shape : {rays.shape}") # !!! rays.shape : torch.Size([9482, 8])
 
-
+        print(f"near.shape : {near.shape}")
         z_steps = torch.linspace(0, 1, steps=64, device=rays_o.device)
         print(f"t_vals : {z_steps}, rays_o.device : {rays_o.device}")
-        z_vals = near() * (1.-z_steps) + far() * (z_steps)
+        z_vals = near * (1.-z_steps) + far * (z_steps)
         z_vals = z_vals.expand([N_vertices, 64])
 
-        pts = rays_o()[...,None,:] + rays_d()[...,None,:] * z_vals()[...,:,None]
+        pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None]
         
         sh = rays_d.shape # [..., 3] ->확인됨
         # print(f"### sh.shape : {sh}")
